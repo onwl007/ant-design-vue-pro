08a5a3dc6b62c5c2b58c5b3f35f862bd19af8575
onwl007 <onwl007@126.com>
2019-04-27 12:21:29 +0800

    初始化路由设置

---
+++ src/views/User/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">登录页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css"></style>
---
+++ src/views/User/Register.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">注册页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css"></style>
